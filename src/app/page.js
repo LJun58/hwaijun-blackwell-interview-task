665e58e39c8b86ae3ff92ec77abd28b0cf01cb86
JUN <hwaijun@hotmail.com>
2025-04-29 14:15:51 +0800

    fix: enhance herosection UIUX

--- src/app/page.js
+++ src/app/page.js
@@ -24,6 +24,7 @@ export default function Home() {
         margin: "0 auto",
         paddingTop: "5%",
         boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)",
+        overflow: "hidden",
       }}
     >
       <HeroSection />
